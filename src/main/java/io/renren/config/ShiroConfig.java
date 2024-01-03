02cf5f9319a8f5d90a7ca7846357ae5ff9254140
xhy-14 <74343154+xhy-14@users.noreply.github.com>
2024-01-03 19:48:47 +0800

    修改shiro filter

--- src/main/java/io/renren/config/ShiroConfig.java
+++ src/main/java/io/renren/config/ShiroConfig.java
@@ -60,6 +60,7 @@ public class ShiroConfig {
         filterMap.put("/swagger-resources/**", "anon");
         filterMap.put("/captcha.jpg", "anon");
         filterMap.put("/aaa.txt", "anon");
+        filterMap.put("/common/**", "anon");
         filterMap.put("/**", "jwt");
         shiroFilter.setFilterChainDefinitionMap(filterMap);
 
--- src/main/java/io/renren/modules/chart/controller/ChartTypeController.java
+++ src/main/java/io/renren/modules/chart/controller/ChartTypeController.java
@@ -3,13 +3,10 @@ package io.renren.modules.chart.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import org.apache.shiro.authz.annotation.RequiresGuest;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.renren.modules.chart.entity.ChartTypeEntity;
 import io.renren.modules.chart.service.ChartTypeService;
